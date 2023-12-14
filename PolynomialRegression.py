532f4efd81f2d0f173658577172a93b4d12153b8
Jayd-H <holdsworthjayd@gmail.com>
2023-12-14 07:33:01 +0000

    Added the MSE and the R2 to the figure

--- PolynomialRegression.py
+++ PolynomialRegression.py
@@ -26,13 +26,35 @@ y_pred = poly_regressor.predict(x_test_poly)
 # Evaluate the model
 r2 = r2_score(y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
-print(f'R² Score: {r2}')
-print(f'Mean Squared Error: {mse}')
+
+# Get feature importance (coefficients) from the polynomial regressor
+feature_importances = np.abs(poly_regressor.coef_[0])
+
+# Start subplots
+plt.figure(figsize=(14, 6))
 
 # Scatter plot of actual vs predicted values
+ax1 = plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
 plt.scatter(y_test, y_pred, color='blue')
 plt.xlabel('Actual Sale Prices')
 plt.ylabel('Predicted Sale Prices')
 plt.title('Actual vs Predicted Sale Prices (Polynomial Regression)')
 plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=4)
+
+# Annotating the R2 and MSE on the scatter plot
+textstr = f'R² Score: {r2:.2f}\nMSE: {mse:.2e}'
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+# Place the text box in upper left in axes coords
+ax1.text(0.05, 0.95, textstr, transform=ax1.transAxes, fontsize=12,
+         verticalalignment='top', bbox=props)
+
+# Feature Importance Plot
+ax2 = plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+sorted_idx = np.argsort(feature_importances)[-10:]  # Get indices of top 10 features
+plt.barh(range(10), feature_importances[sorted_idx], color='blue')
+plt.xlabel('Feature Importance (Coefficient Magnitude)')
+plt.title('Top 10 Most Important Features (Polynomial Regression)')
+plt.yticks(range(10), poly.get_feature_names_out()[sorted_idx])
+
+plt.tight_layout()  # Adjust the layout
 plt.show()
