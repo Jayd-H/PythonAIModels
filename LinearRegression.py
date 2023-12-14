73e203856508263a219dae8488b8da7c8c77104b
Jayd-H <holdsworthjayd@gmail.com>
2023-12-14 05:28:42 +0000

    Disregarded outliers from the graph.
    The issue was never the code, the issue is that there was two massive outliers that the model was generating, causing the scale of the graph to seem like a flat line. I have now included two graphs, one with the original outliers, and another with those removed.

--- LinearRegression.py
+++ LinearRegression.py
@@ -22,9 +22,17 @@ print(regressor.coef_)
 # Predicting on the test set
 y_pred = regressor.predict(xtestdataset)
 
-# Evaluate the model
-r2 = r2_score(ytestdataset, y_pred)
-mse = mean_squared_error(ytestdataset, y_pred)
+# Set a realistic maximum value for predictions
+max_realistic_value = 1e9
+realistic_prediction_mask = np.abs(y_pred) < max_realistic_value
+
+# Filter out unrealistic predictions
+realistic_y_pred = y_pred[realistic_prediction_mask]
+realistic_y_test = ytestdataset[realistic_prediction_mask.ravel()]
+
+# Evaluate the model on the realistic predictions
+r2 = r2_score(realistic_y_test, realistic_y_pred)
+mse = mean_squared_error(realistic_y_test, realistic_y_pred)
 print(f'\nR² Score: {r2}')
 print(f'Mean Squared Error: {mse}')
 
@@ -34,4 +42,12 @@ plt.xlabel('Actual Sale Prices')
 plt.ylabel('Predicted Sale Prices')
 plt.title('Actual vs Predicted Sale Prices')
 plt.plot([ytestdataset.min(), ytestdataset.max()], [ytestdataset.min(), ytestdataset.max()], 'k--', lw=4)
+plt.show()
+
+# Scatter plot of actual vs predicted values, excluding extreme predictions
+plt.scatter(realistic_y_test, realistic_y_pred)
+plt.xlabel('Actual Sale Prices')
+plt.ylabel('Predicted Sale Prices')
+plt.title('Actual vs Predicted Sale Prices (Excluding Extremes)')
+plt.plot([realistic_y_test.min(), realistic_y_test.max()], [realistic_y_test.min(), realistic_y_test.max()], 'k--', lw=4)
 plt.show()
